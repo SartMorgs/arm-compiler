d166220a7e433615230fa2db1a16dc5b0f640ae3
sartmorg <morgana.sartor@outlook.com>
2021-05-26 02:27:43 -0300

    add binary lits for all instructions

--- armcompiler/translator/ArmTranslator.py
+++ armcompiler/translator/ArmTranslator.py
@@ -1,40 +1,42 @@
 # Arm Translator
 
-class ArmTranslater():
-	opcode = {
-		'ADDS': ('000001', '1', '16')
-		'SUBS': ('000010', '1', '16')
-		'MULS': ('000011', '1', '16')
-		'ANDS': ('000100', '1', '16')
-		'ORRS': ('000101', '1', '16')
-		'EORS': ('000110', '1', '16')
-		'BICS': ('000111', '1', '16')
-		'ASRS': ('001000', '1', '16')
-		'LSLS': ('001001', '1', '16')
-		'LSRS': ('001010', '1', '16')
-		'RORS': ('001011', '1', '16')
-		'CMN': 	('001100', '2', '8')
-		'CMP': 	('001101', '2', '8')
-		'MOVS': ('001110', '4', '')
-		'BEQ': 	('001111', '3', '26')
-		'BNE': 	('010000', '3', '26')
-		'BLT': 	('010001', '3', '26')
-		'BL': 	('010010', '3', '26')
-		'BX': 	('010011', '3', '26')
-		'LDR': 	('010100', '4', '14')
-		'STR': 	('010101', '4', '14')
-		'NOP': 	('111111', '5', '0')
-	}
+class ArmTranslator():
+
+	def __init__(self):
+		self.opcode = {
+			'ADDS': ('000001', '1', '16'),
+			'SUBS': ('000010', '1', '16'),
+			'MULS': ('000011', '1', '16'),
+			'ANDS': ('000100', '1', '16'),
+			'ORRS': ('000101', '1', '16'),
+			'EORS': ('000110', '1', '16'),
+			'BICS': ('000111', '1', '16'),
+			'ASRS': ('001000', '1', '16'),
+			'LSLS': ('001001', '1', '16'),
+			'LSRS': ('001010', '1', '16'),
+			'RORS': ('001011', '1', '16'),
+			'CMN': 	('001100', '2', '8'),
+			'CMP': 	('001101', '2', '8'),
+			'MOVS': ('001110', '4', ''),
+			'BEQ': 	('001111', '3', '26'),
+			'BNE': 	('010000', '3', '26'),
+			'BLT': 	('010001', '3', '26'),
+			'BL': 	('010010', '3', '26'),
+			'BX': 	('010011', '3', '26'),
+			'LDR': 	('010100', '4', '14'),
+			'STR': 	('010101', '4', '14'),
+			'NOP': 	('111111', '5', '0')
+		}
 
 	def get_register(self, reg):
-		reg = int(register.replace('R', ''))
-		binary = bin(reg).replace('0b', '')
+		register = int(reg.replace('R', ''))
+		binary = bin(register).replace('0b', '')
 		while len(binary) < 5:
 			binary = '0' + binary
 		return binary
 
-	def get_number(self, value, optype):
-		length = int(optype)
+	def get_number(self, value, optype='NOP'):
+		length = int(self.opcode[optype][2])
 		number = int(value)
 		binary = bin(number).replace('0b', '')
 		while len(binary) < length:
@@ -42,7 +44,18 @@ class ArmTranslater():
 		return binary
 
 	def get_instruction_body(self, expression):
+		optype = 'NOP'
+		code = []
 		for exp in expression:
-			if isinstance(exp, tuple):
-				registers = [get_register(field) for field in exp if field.contains('R')]
-				registers = [get_number(field) for field in exp if !field.contains('R')]
+			instruction = []
+			for inst in exp:
+				dict_keys = [key for key in self.opcode.keys()]
+				if inst in dict_keys:
+					optype = inst
+					instruction.append(self.opcode[optype][0])
+				else:
+					body_instruction = [self.get_register(field) if 'R' in field else self.get_number(field, optype) for field in inst]
+					for item in body_instruction:
+						instruction.append(item)
+			code.append(instruction)
+		print(code)
---
+++ codetest/commandtest.py
@@ -0,0 +1,9 @@
+from armcompiler.parser.ArmYacc import *
+y = ArmSyntaticPatternParser()
+y.build()
+code = ['LDR R0, 35;', 'LDR R1, 12;', 'ADDS R2, R0, R1;', 'SUBS R3, R0, R1;']
+expr = y.parsing(code)
+
+from armcompiler.translator.ArmTranslator import *
+t = ArmTranslator()
+t.get_instruction_body(expr)
